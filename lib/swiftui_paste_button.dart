5f73eb5689b504660badb2ea29dd1846a2e4df40
Morgan McKenzie <rmtmckenzie@gmail.com>
2025-12-04 00:17:02 -0800

    Try adding eager gesture recognition to paste button

--- lib/swiftui_paste_button.dart
+++ lib/swiftui_paste_button.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/services.dart';
 import 'package:swiftui_paste_button/paste_controller.dart';
 import 'package:swiftui_paste_button/pigeon/pigeon.g.dart';
-
-
 
 class SwiftUIPasteButton extends StatefulWidget {
   const SwiftUIPasteButton({super.key, this.params, required this.onPaste});
@@ -16,7 +16,6 @@ class SwiftUIPasteButton extends StatefulWidget {
 }
 
 class _SwiftUIPasteButtonState extends State<SwiftUIPasteButton> {
-
   int? platformId;
 
   @override
@@ -34,30 +33,33 @@ class _SwiftUIPasteButtonState extends State<SwiftUIPasteButton> {
     final size = MediaQuery.sizeOf(context);
 
     // this prevents a crash if a size constraint not given
-    return LayoutBuilder(builder: (context, constraints) {
-      return ConstrainedBox(
-        constraints: BoxConstraints(
-          maxWidth: constraints.hasInfiniteWidth ? size.width: constraints.maxWidth,
-          maxHeight: constraints.hasInfiniteHeight ? size.height: constraints.maxHeight,
-          minWidth: 0,
-          minHeight: 0,
-        ),
-        child: UiKitView(
-          viewType: viewType,
-          layoutDirection: TextDirection.ltr,
-          creationParams: widget.params?.encode(),
-          creationParamsCodec: MessageFlutterApi.pigeonChannelCodec,
-          onPlatformViewCreated: (id) {
-            // if not mounted, it has already returned so let's ignore
-            if (context.mounted) {
-              platformId = id;
-              PasteController.registerCallback(id, widget.onPaste);
-            }
-          },
-        ),
-      );
-    });
-
-
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return ConstrainedBox(
+          constraints: BoxConstraints(
+            maxWidth: constraints.hasInfiniteWidth ? size.width : constraints.maxWidth,
+            maxHeight: constraints.hasInfiniteHeight ? size.height : constraints.maxHeight,
+            minWidth: 0,
+            minHeight: 0,
+          ),
+          child: UiKitView(
+            viewType: viewType,
+            layoutDirection: TextDirection.ltr,
+            creationParams: widget.params?.encode(),
+            creationParamsCodec: MessageFlutterApi.pigeonChannelCodec,
+            onPlatformViewCreated: (id) {
+              // if not mounted, it has already returned so let's ignore
+              if (context.mounted) {
+                platformId = id;
+                PasteController.registerCallback(id, widget.onPaste);
+              }
+            },
+            gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>{
+              Factory<OneSequenceGestureRecognizer>(() => EagerGestureRecognizer()),
+            },
+          ),
+        );
+      },
+    );
   }
 }
